32a574e98ce3dc5ae91f1b84ce6448f7571862b4
Irvin Szeto <irvins@stanford.edu>
2018-11-13 13:20:07 -0800

    testing sucess

--- WellReminders.php
+++ WellReminders.php
@@ -18,7 +18,7 @@ class WellReminders extends \ExternalModules\AbstractExternalModule
     public function startCron() {
         $this->emDebug("Cron Args",func_get_args());
 
-        $start_times = array("11:30","13:18","13:20","13:22","13:24","13:26","13:28","13:30");
+        $start_times = array("11:30","13:30");
         $run_days    = array("sun","mon");
         $cron_freq = 3600; //weekly
 
